ec0dfaf1f9c30746a8d96006f2244414f1a84326
Eszter <szteran@gmail.com>
2024-01-11 11:46:12 +0100

    added mui

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,10 +1,61 @@
-function Pokemon({ pokemon }) {
-	console.log(pokemon)
+import { useEffect, useState } from "react"
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
 
+
+function Pokemon({ pokemon }) {
+	const [pokeData, setPokeData] = useState(null)
+	//let pokeData = null
+	
+	//console.log(pokemon.url)
+
+	useEffect(()=> console.log(pokeData), [pokeData]) //figyeljük így a statet. Itt: nézd a (pokeDatát), és ha változik, akkor írd ki a [pokeDatát]
+
+	//nem változókba mentjük a fetch adatait, mert infinite loop-ra fut.
+	useEffect(() => {
+		fetch(pokemon.url)
+			.then(res => res.json())
+			.then(data => { 
+			//pokeData = data
+			setPokeData(data)
+			//console.log("fetch finished: ", pokeData)
+		})
+	}, [pokemon.url])
+
+	console.log("loading component")
 	return (
-		<div className="pokemon">
-			{pokemon.name}
-		</div>
+		<Card 
+			className="pokemon"
+			sx={{ width: 200}}
+		>
+
+		<CardHeader
+			title ={pokemon.name}
+		/>
+
+		{pokeData &&
+			<> 
+			<CardMedia
+			component="img"
+			image={pokeData.sprites.front_default}
+			alt={pokemon.data}
+			/>
+
+			<CardContent>
+				<p>weight: {pokeData.weight} kg</p>
+				<p>height: {pokeData.height} cm</p>
+			</CardContent>
+
+			{/* {pokemon.name} */}
+			{/* {pokeData.sprites.front_default} */}
+				{/* {pokeData.weight} kg 
+				{pokeData.height} cm
+				<img src={pokeData.sprites.front_default} /> */}
+			</>
+		}
+		</Card>
 	)
 }
 
--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -1,8 +1,12 @@
+//import Button from '@mui/material/Button';
 import Pokemon from "./Pokemon"
+import './Pokemons.css'
+
 
 function Pokemons({ pokemons }) {
 	return (
 		<div className="pokemons">
+			
 			{pokemons.map((pokemon, index) => <Pokemon key={index} pokemon={pokemon} />)}
 		</div>
 	)
